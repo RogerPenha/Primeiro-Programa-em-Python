fded1e1a9524e5928bba4d3e142915a4aaf859a8
unknown <roger.penhasenai@gmail.com>
2023-03-16 16:19:02 -0300

    adicionado sistema para calcular a idade das pessoas

---
+++ idade.py
@@ -0,0 +1,8 @@
+nascimento = 2006
+anoatual = 2023
+idade = anoatual-nascimento
+
+def calculoidade(nascimento, anoatual):
+    idade = anoatual-nascimento
+    return idade
+print(f"a idade da pessoa é {idade} anos")
